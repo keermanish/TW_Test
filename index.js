40331961096d0578d10fcaacbacf1fdf06a4439c
Keer <makeer@deloitte.com>
2019-01-28 20:46:53 +0530

    imported all scripts into index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // import all files/scripts
-// require('./src/FetchChallenge');
+require('./src/FetchChallenge');
 require('./src/Challenge1');
+require('./src/Challenge2');
+require('./src/Challenge3');
+require('./src/Challenge4');
