0a1f07618d34dc0f8b01727346f80c09112a8a64
Keer <makeer@deloitte.com>
2019-01-28 20:45:56 +0530

    minor code refactor in challenge1 script

--- src/Challenge1.js
+++ src/Challenge1.js
@@ -2,6 +2,11 @@ const fetch = require('node-fetch');
 const helper = require('./common/Helper');
 const { BASE_URL, USER_ID } = require('./common/Const');
 
+/**
+ * function to read and send the product count
+ * @param {Array} data - all product information
+ * @returns {Promise}
+ */
 function sendProductCount(data) {
   const output = { count: data.length };
 
@@ -9,24 +14,35 @@ function sendProductCount(data) {
     method: 'post',
     body: JSON.stringify({ output }),
     headers: {
+      UserID: USER_ID,
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(data => data)
+    .catch(helper.error);
+}
+
+/**
+ * function to fetch all products
+ * @returns {Promise} - list of all products
+ */
+function getAllProducts() {
+  return fetch(`${BASE_URL}/challenge/input`, {
+    method: 'get',
+    headers: {
       UserID: USER_ID
     }
   })
-  .then(response => response.json())
-  .then(data => data)
-  .catch(helper.error);
+    .then(response => response.json())
+    .catch(helper.error);
 }
 
-// get the input data and post count of all products
-fetch(`${BASE_URL}/challenge/input`, {
-  method: 'get',
-  headers: {
-    UserID: USER_ID
-  }
-})
-.then(response => response.json())
-.then(sendProductCount)
-.then(data => {
-  helper.log(data);
-})
-.catch(helper.error);
+/* init, get all products and start process */
+(function() {
+  getAllProducts()
+    .then(sendProductCount)
+    .then(helper.log)
+    .catch(helper.error);
+})();
--- src/FetchChallenge.js
+++ src/FetchChallenge.js
@@ -2,15 +2,22 @@ const fetch = require('node-fetch');
 const helper = require('./common/Helper');
 const { BASE_URL, USER_ID } = require('./common/Const');
 
-// first get all challenges
-fetch(`${BASE_URL}/challenge`, {
-  method: 'get',
-  headers: {
-    UserID: USER_ID
-  }
-})
-.then(response => response.json())
-.then(data => {
-  helper.log(data);
-})
-.catch(helper.error);
+/**
+ * function to fetch latest challenge
+ * @returns {Promise}
+ */
+function fetchLatestChallenge() {
+  return fetch(`${BASE_URL}/challenge`, {
+    method: 'get',
+    headers: {
+      UserID: USER_ID
+    }
+  })
+  .then(response => response.json())
+  .then(helper.log)
+  .catch(helper.error);
+}
+
+(function() {
+  fetchLatestChallenge();
+})();
